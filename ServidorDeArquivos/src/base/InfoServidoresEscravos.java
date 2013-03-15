42078434ab50b918cbdb36d3db44a462e61e71b8
Jorge Augusto <jorge_programador@hotmail.com>
2013-03-15 03:49:17 -0300

    Enviando os arquivos iniciais do projeto.

--- ServidorDeArquivos/src/base/InfoServidoresEscravos.java
+++ ServidorDeArquivos/src/base/InfoServidoresEscravos.java
@@ -27,7 +27,5 @@ public class InfoServidoresEscravos implements Serializable{
     @Override
     public String toString() {
         return "[" + nome + ", " + ip + ", " + porta + "]";
-
-
     }
 }
